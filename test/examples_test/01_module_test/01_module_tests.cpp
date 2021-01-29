01e06806a7a00386c5a78a1221f70e6a3de90f55
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-01-28 19:05:42 -0600

    Example input get total

--- test/examples_test/01_module_test/01_module_tests.cpp
+++ test/examples_test/01_module_test/01_module_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "vars.h"
+#include "input.h"
 #include<string>
 
 TEST_CASE("Verify Test Configuration", "verification") {
@@ -11,6 +12,12 @@ TEST_CASE("Verify Test Configuration", "verification") {
 TEST_CASE("Verify the echo variable function", "should return the value of param") 
 {
 	REQUIRE(echo_variable(5) == 5);
+}
+
+TEST_CASE("Verify the get total function", "should return product of params") 
+{
+	REQUIRE(get_total(5, 6) == 30);
+	REQUIRE(get_total(5, 10) == 50);
 }
 
 /*test case add_to_double_1 with 0 as parameter*/
