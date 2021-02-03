860a43be357e15e44a5583a6faa4482758f82ea9
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-02-02 20:10:26 -0600

    Example: operator precedence

--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -1,13 +1,23 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "../src/examples/02_module/01_expressions/expressions.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
 /*Test case operator precedence 1 with function argument values 12, 6 and 3 returns 14*/
+TEST_CASE("Verify operator precedence 1", "verify order of ops") 
+{
+	REQUIRE(operator_precedence_1(12, 6, 3) == 14);
+}
 
 /*Test case operator precedence 2 with function argument values 12, 6 and 3 returns 6*/
+TEST_CASE("Verify operator precedence 2", "verify order of ops") 
+{
+	REQUIRE(operator_precedence_2(12, 6, 3) == 6);
+}
+
 
 /*Test case operator precedence 3 with function argument values 12, 6 and 3 returns 6*/
 
