2757bd6e8a2bb8b69bdf18c8cbceaf3921c694eb
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-02-08 19:08:21 -0600

    cast, overflow. const, test cases

--- src/examples/02_module/02_casting/main.cpp
+++ src/examples/02_module/02_casting/main.cpp
@@ -1,6 +1,16 @@
+#include "casting.h"
+#include <iostream>
+
+using std::cout;  using std::cin;
 
 int main()
 {
+    auto num = .01;
+    cout<<"Enter a decimal number: ";
+    cin>>num;
+
+    cout<<"You typed: "<<num<<"\n";
+    cout<<"Int Converted to double:  "<<convert_double_to_int(num);
 
     return 0;
 }
--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -1,6 +1,10 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include <climits>
 #include "../src/examples/02_module/01_expressions/expressions.h"
+#include "casting.h"
+#include "overflow.h"
+#include "constants.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -24,16 +28,42 @@ TEST_CASE("Verify operator precedence 2", "verify order of ops")
 /*Test case operator precedence 3 with function argument values 12, 6 and 3 returns 6*/
 
 /*Test case convert_to_double to show that multiplying int and double returns a double*/
+TEST_CASE("Test convert to double", "int * double, int converted to double")
+{
+	REQUIRE(convert_to_double(10, .35) == 3.5);
+}
 
 /*Test case convert double to int to show that a double converted to int truncates 
 the decimal portion*/
+TEST_CASE("Test convert to double to int", "double to int, truncate decimals")
+{
+	REQUIRE(convert_double_to_int(10.375) == 10);
+	REQUIRE(convert_double_to_int(10.875) == 10);
+}
 
 /*Test case static cast double int to show that a double casted to int truncates
 decimal portion*/
+TEST_CASE("Test static cast double to int", "explicit double to int, truncate decimals")
+{
+	REQUIRE(static_cast_double_int(10.375) == 10);
+}
 
 /*Test int overflow to show adding 1 to 2147483647 returns -2147483648*/
+TEST_CASE("Test int overflow", "Create C++ weirdness")
+{
+	REQUIRE(int_overflow() == -2147483648);
+}
+
+TEST_CASE("Test double underflow", "Decimal havoc")
+{
+	REQUIRE(double_underflow() == 0);
+}
 
 /*Test get radius of a circle with 10 to show that it returns 314.159*/
+TEST_CASE("Test get area of circle function", "use constants ")
+{
+	REQUIRE(get_area_of_circle(10) == 314.159);
+}
 
 /*Test int data size */
 
