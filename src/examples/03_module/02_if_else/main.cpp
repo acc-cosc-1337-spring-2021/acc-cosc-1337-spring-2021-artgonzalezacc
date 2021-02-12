3c26bd11c124973ab9bcff52e00c399a46bfc4ce
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-02-11 18:14:47 -0600

    if else if main, switch main

--- src/examples/03_module/02_if_else/main.cpp
+++ src/examples/03_module/02_if_else/main.cpp
@@ -12,7 +12,13 @@ Pass the year to get_generation function and display function result.
 */
 int main() 
 {
-	
+	auto year = 1900;
+	cout<<"Enter year: ";
+	cin>>year;
+
+	auto generation = get_generation(year);
+
+	cout<<"Generation is: "<<generation;
 	
 	return 0;
 }
