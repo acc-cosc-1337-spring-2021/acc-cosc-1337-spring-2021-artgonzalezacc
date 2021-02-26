2d9e8d66d7f986f5b240fffe8c91ed76ec24c51a
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-02-25 19:05:25 -0600

    for loop and for ranged, loop string

--- src/examples/04_module/04_for_ranged/for_ranged.h
+++ src/examples/04_module/04_for_ranged/for_ranged.h
@@ -1,10 +1,11 @@
+#include<iostream>
 #include<string>
 #include<vector>
 
 //write a void function prototype for loop_string_w_index that accepts a string parameter
-
+void loop_string_w_index(std::string str);
 
 
 //write a void function prototype for loop_string_w_for_range  that accepts a string parameter
-
+void loop_string_w_for_range(std::string str);
 
--- src/examples/04_module/04_for_ranged/main.cpp
+++ src/examples/04_module/04_for_ranged/main.cpp
@@ -1,9 +1,10 @@
-#include<string>
-#include<iostream>
 #include "for_ranged.h"
 
 int main() 
 {
-
+	std::string name = "Joe";
+	//loop_string_w_index(name);
+	loop_string_w_for_range(name);
+	
 	return 0;
 }
