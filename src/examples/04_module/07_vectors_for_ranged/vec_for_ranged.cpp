db44b28075e64407c706b83cb0e508fae83e2936
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-03-23 19:59:52 -0500

    vector examples

--- src/examples/04_module/07_vectors_for_ranged/vec_for_ranged.cpp
+++ src/examples/04_module/07_vectors_for_ranged/vec_for_ranged.cpp
@@ -1,7 +1,6 @@
 #include "for_ranged.h"
-#include<iostream>
-#include<vector>
 
+using std::cout;
 using std::vector;
 
 /*
@@ -20,8 +19,23 @@ Displays the numbers to screen using.
 
 @param vector<int>
 */
+void loop_vector_w_for_ranged(std::vector<int> nums)
+{
+    for(auto num: nums)
+    {
+        cout<<num<<"\n";
+        num = -1;
+    }
+    
+    cout<<"\n\n vector results\n";
 
+    for(auto num: nums)
+    {
+        cout<<num<<"\n";
+        
+    }
 
+}
 
 /*
 Function loop_vector_w_for_ranged_ref
@@ -30,6 +44,23 @@ Displays the numbers to screen
 
 @param vector<int>&
 */
+void loop_vector_w_for_ranged_ref(std::vector<int>& nums)
+{
+    for(auto& num: nums)
+    {
+        cout<<num<<"\n";
+        num = -1;
+    }    
+
+    cout<<"\n\n REF vector results\n";
+
+    for(auto num: nums)
+    {
+        cout<<num<<"\n";
+        
+    }
+
+}
 
 
 /*
@@ -40,3 +71,12 @@ Displays the numbers to screen
 @param const vector<int>&
 */
 
+vector<int> get_vector()
+{
+    vector<int> nums;
+    nums.push_back(5);
+    nums.push_back(1);
+    nums.push_back(10);//5, 1, 10
+
+    return nums;
+}
--- src/examples/04_module/07_vectors_for_ranged/vec_for_ranged.h
+++ src/examples/04_module/07_vectors_for_ranged/vec_for_ranged.h
@@ -11,16 +11,17 @@ Write function prototype for void function loop_vector_w_for_ranged with no para
 Write function prototype for void function loop_vector_w_for_ranged_val with a vector of int pass by value parameter.
 
 */
+void loop_vector_w_for_ranged(std::vector<int> nums);
 
 
 
 /*
 Write function prototype for void function loop_vector_w_for_ranged_ref with a vector of int pass by reference parameter.
 */
-
+void loop_vector_w_for_ranged_ref(std::vector<int>& nums);
 
 /*
 Write function prototype for void function loop_vector_w_for_ranged_const_ref with a vector of int const pass by reference parameter.
 */
 
-
+std::vector<int> get_vector();
