e74847ecb51d32b1d463b65d54d31b85a1605fb3
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-03-23 20:00:59 -0500

    bank account class: access specifier, var, func

--- src/examples/06_module/01_bank/bank_account.h
+++ src/examples/06_module/01_bank/bank_account.h
@@ -1,1 +1,8 @@
 //bank_account.h
+class BankAccount
+{
+public://access specifier
+    int get_balance()const {return balance;}
+private:
+    int balance{0};
+};
--- src/examples/06_module/01_bank/main.cpp
+++ src/examples/06_module/01_bank/main.cpp
@@ -1,6 +1,15 @@
+#include<iostream>
+#include<string>
+using std::string;
+using std::cout;
+#include "bank_account.h"
 
 int main()
 {
+	string name;//variable
+
+	BankAccount account;//variable-an instance of BankAccount object
+	cout<<"Balance: "<<account.get_balance()<<"\n";
 
 	return 0;
 }
