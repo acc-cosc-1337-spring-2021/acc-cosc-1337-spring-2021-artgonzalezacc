a50b3ca648207014606640278bd8c523eb6735b5
artgonzalezacc <arturo.gonzalez@austincc.edu>
2021-05-18 08:26:04 -0500

    config workflow

--- .github/script.sh
+++ .github/script.sh
@@ -4,5 +4,5 @@ mkdir -p ../build;
 cd ../build; 
 cmake ../acc-cosc-1337-spring-2021-artgonzalezacc/; 
 make;
-cd test/examples_test/06_module_test;
-./ex_06_tests -s;
+cd test/examples_test/09_module_test;
+./ex_09_tests -s;
